bed354369828f8acce92aa9d70d716c636a18022
Lee Bussy <lee@bussy.org>
2020-03-01 11:56:06 -0600

    Working with Async

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,41 +1,44 @@
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+// Basic.ino
+// Simple autoConnect usage example
+
+#define WM_ASYNC // Turn on Async mode
+#include "AsyncWiFiManager.h" // https://github.com/lbussy/AsyncWiFiManager
 
 void setup() {
-    WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+    // Put your setup code here, to run once:
 
-    // put your setup code here, to run once:
-    Serial.begin(115200);
+    Serial.begin(BAUD);
+
+    WiFi.mode(WIFI_STA); // Explicitly set mode, ESP defaults to STA+AP
     
-    // WiFi.mode(WiFi_STA); // it is a good practice to make sure your code sets wifi mode how you want it.
+    // AsyncWiFiManager, Local intialization. Once its business is done,
+    // there is no need to keep it around
+    AsyncWiFiManager wm;
 
-    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wm;
-
-    //reset settings - wipe credentials for testing
-    //wm.resetSettings();
+    // Reset settings - wipe credentials for testing
+    // wm.resetSettings();
 
     // Automatically connect using saved credentials,
-    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
-    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
-    // then goes into a blocking loop awaiting configuration and will return success result
+    // if connection fails, it starts an access point with the specified name
+    // ( "AutoConnectAP"), if empty will auto generate SSID, if password is
+    // blank it will be an anonymous AP (wm.autoConnect()) then goes into a
+    // blocking loop awaiting configuration and will return success result.
 
     bool res;
-    // res = wm.autoConnect(); // auto generated AP name from chipid
-    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+    // res = wm.autoConnect(); // Auto generated AP name from chipid, anonymous
+    res = wm.autoConnect("AutoConnectAP"); // Anonymous AP
+    // res = wm.autoConnect("AutoConnectAP","password"); // Password protected named AP
 
     if(!res) {
-        Serial.println("Failed to connect");
+        Serial.println("Failed to connect.");
         // ESP.restart();
     } 
     else {
         //if you get here you have connected to the WiFi    
-        Serial.println("connected...yeey :)");
+        Serial.println("Connected.");
     }
-
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    
+    // Put your main code here, to run repeatedly:
 }
